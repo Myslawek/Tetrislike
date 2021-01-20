cd8a2817b21d3e5943bbc8f9bd215f37c0acb1dd
Myslawek <przemek.g96@wp.pl>
2021-01-20 16:13:18 +0100

    Add basic colors and management function

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ import pygame
 import random
 import numpy
 import dataclasses
+
+colors = {
+    "RED": (255, 0, 0),
+    "GREEN": (0, 255, 0),
+    "BLUE": (0, 0, 255),
+    "YELLOW": (255, 255, 0),
+    "MAGENTA": (255, 0, 255),
+    "CYAN": (0, 255, 255),
+    "BLACK": (0, 0, 0),
+    "GRAY": (128, 128, 128),
+    "WHITE": (255, 255, 255)
+}
+
+
+def index_to_color(color_index: int) -> typing.Tuple:
+    try:
+        return colors.get(list(colors)[int(color_index)])
+    except:
+        print(f'Failed at provided: {type(color_index)}, with value {color_index}')
